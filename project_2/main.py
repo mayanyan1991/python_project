c0644a895aa84044302393aeb67d1cc4113ce74f
Yan <mayan19911114@gmail.com>
2022-09-02 13:53:43 +0200

    update project 2

--- project_2/main.py
+++ project_2/main.py
@@ -3,6 +3,7 @@ import random
 def guess(x):
     a = random.randint(1,x)
     b = int(input("guess a number between 1 and "+ str(x) +": "))
+    # alternative for input (f"guess....{x}")
     min_limi = 1
     max_limi = x
     while a > 1:
@@ -17,7 +18,22 @@ def guess(x):
             break
         else:
             b = int(input("please type in correct number: "))
-            
- 
-guess(10)
+
+def guess_2(x):
+    a = random.randint(1,x)
+    b = 0
+    # alternative for input (f"guess....{x}")
+    min_limi = 1
+    max_limi = x
+    while a != b:
+        b = int(input("guess a number between " + str(min_limi) + "  and " + str(max_limi) +": ")) 
+        if b > a and b <= max_limi:
+            max_limi = b 
+        elif b < a and b >= min_limi:
+            min_limi = b
+        elif b < min_limi or b > max_limi:
+            b = int(input("please type in correct number: "))           
+    print("correct")
+
+guess_2(10)
 
